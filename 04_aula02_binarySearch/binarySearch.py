67928c5dcf15fe3d381b4942cd33300dd111cf67
fdelgadov <fdelgadov@unsa.edu.pe>
2021-10-03 19:16:23 -0500

    Aula02: Se corrigió binarySearch.py
    Se corrigió el bucle producidio cuando max < min

--- 04_aula02_binarySearch/binarySearch.py
+++ 04_aula02_binarySearch/binarySearch.py
@@ -1,16 +1,18 @@
+import time
+
 def binarySearch(array, search):
   min = 0
   max = len(array) - 1
   pivotIdx = -1
   while True:
-    # print(f"{min} {max} {pivotIdx}") # debug
-    if max == min:
+    #print(f"{min} {max} {pivotIdx}") # debug
+    if max <= min:
       if search == array[max]:
         return True
       else:
         return False
     pivotIdx = int((max + min)/2)
-    # print(f"{min} {max} {pivotIdx}") # debug
+    #print(f"{min} {max} {pivotIdx}") # debug
     if search < array[pivotIdx]:
       max = pivotIdx - 1
     elif search > array[pivotIdx]:
@@ -22,3 +24,33 @@ def binarySearch(array, search):
 a = [1, 4, 8, 12, 16, 19, 22, 26, 29, 30, 37, 44]
 print(binarySearch(a, 25))
 print(binarySearch(a, 16))
+
+a = [x for x in range(1, 10000)]
+tic = time.perf_counter()
+print(binarySearch(a, 4265))
+tac = time.perf_counter()
+print(f"Binary Search 10000: {tac - tic:0.4f} segundos")
+
+a = [x for x in range(3, 100000)]
+tic = time.perf_counter()
+print(binarySearch(a, 42650))
+tac = time.perf_counter()
+print(f"Binary Search 100000: {tac - tic:0.4f} segundos")
+
+a = [x for x in range(1, 100000)]
+tic = time.perf_counter()
+print(binarySearch(a, 42653))
+tac = time.perf_counter()
+print(f"Binary Search 100000: {tac - tic:0.4f} segundos")
+
+a = [x for x in range(1, 1000000)]
+tic = time.perf_counter()
+print(binarySearch(a, 426533))
+tac = time.perf_counter()
+print(f"Binary Search 1000000: {tac - tic:0.4f} segundos")
+
+a = [x for x in range(1, 100000000)]
+tic = time.perf_counter()
+print(binarySearch(a, 42653334))
+tac = time.perf_counter()
+print(f"Binary Search 100000000: {tac - tic:0.4f} segundos")
