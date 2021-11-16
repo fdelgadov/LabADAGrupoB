6d36a0c3c9291f66b425519e4e43f1b004f66243
fdelgadov <fdelgadov@unsa.edu.pe>
2021-11-15 20:15:52 -0500

    Aula06: Se desarrolla el algoritmo DFS

---
+++ 10_aula07_DFS/ejercicio1.py
@@ -0,0 +1,27 @@
+def dfs(array, x, y, color):
+  array[x][y] = color
+  dx = [x - 1, x, x + 1, x]
+  dy = [y, y + 1, y, y - 1]
+  for i in range(4):
+    if dx[i] < len(array) and dy[i] < len(array[0]):
+      value = array[dx[i]][dy[i]]
+      if value == '0':
+        dfs(array, dx[i], dy[i], color)
+
+def display(array):
+  for row in array:
+    print(f'{row}')
+  print()
+
+# Test
+array = [['#', '#', '#', '#', '#', '#'],
+         ['#', '0', '0', '0', '0', '#'],
+         ['#', '0', '0', '0', '0', '#'],
+         ['#', '0', '0', '0', '0', '#'],
+         ['#', '0', '0', '0', '#', '0'],
+         ['#', '#', '#', '#', '0', '0']]
+display(array)
+dfs(array, 2, 2, '3')
+display(array)
+dfs(array, 4, 5, '4')
+display(array)
