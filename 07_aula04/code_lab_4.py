d5c2949b06835511a309aa1c14bc308d46a18542
fdelgadov <fdelgadov@unsa.edu.pe>
2021-11-16 18:06:42 -0500

    Aula04: Se modificó el antiguo archivo de respuestas
    - Se comentó parte del código y modificó parte del código para que el editor
      no señale errores
    - El archivo code_lab_4.py contenía todas las respuestas anteriormente, pero
      ahora las respuestas están en archivos individuales

--- 07_aula04/code_lab_4.py
+++ 07_aula04/code_lab_4.py
@@ -35,27 +35,23 @@ def twoSum(array):
 # n = haystack.length
 # m = needle.length
 # O(n*m), porque tenemos un while dentro de un while; es decir, anidado.
-def find_needle(needle, haystack)
+def find_needle(needle, haystack):
     needle_index = 0
     haystack_index = 0
     
-    while haystack_index < haystack.length
-        if needle[needle_index] == haystack[haystack_index]
-            found_needle = true
-            while needle_index < needle.length
-                if needle[needle_index] != haystack[haystack_index + needle_index]
-                    found_needle = false
+    while haystack_index < haystack.length:
+        if needle[needle_index] == haystack[haystack_index]:
+            found_needle = True
+            while needle_index < needle.length:
+                if needle[needle_index] != haystack[haystack_index + needle_index]:
+                    found_needle = False
                 break
-            end
             needle_index += 1
-            end
-        return true if found_needle
+        if found_needle:
+            return True
         needle_index = 0
-        end
         haystack_index += 1
-    end
-    return false
-end
+    return False
 
 # 5. What is the time complexity in terms of O()?
 # n = tamaño del arreglo
@@ -75,15 +71,12 @@ def pick_resume(resumes):
     
     while resumes.length > 1:
         if eliminate == "top":
-            resumes = resumes[resumes.length / 2, resumes.length - 1]
+            resumes = resumes[int(len(resumes) / 2):resumes.length - 1]
             eliminate = "bottom"
         elif eliminate == "bottom":
-            resumes = resumes[0, resumes.length / 2]
+            resumes = resumes[0:int(len(resumes) / 2)]
             eliminate = "top"
-        end
-    end
     return resumes[0]
-end
 
 # Test
 a = [4, 1, 7, 3, 12]
@@ -95,30 +88,30 @@ print(twoSum(a))
 ## Review of Time Complexity
 ## Q1: What is the time complexity of
 ## O(n), es un for de i=0 hasta i=n
-for (i = 0; i < n; i++) {   # n*1 = n = O(n)
-    statement;              # 1
-}
+# for (i = 0; i < n; i++) {   # n*1 = n = O(n)
+#    statement;               # 1
+# }
 
 ## Q2: What is the time complexity of
 ## O(n), porque es un for de i=n hasta i=1
-for (i = n; i > 0; i--) { # n
-    statement;            # 1
-}
+# for (i = n; i > 0; i--) { # n
+#     statement;            # 1
+# }
 
 ## Q3: What is the time complexity of
 # i = 0, 2, 4, 6, ...
 # O(n)
-for (i = 0; i < n; i=i+2) { # n/2*1 = n/2 = O(n)
-    statement;              # 1
-}
+# for (i = 0; i < n; i=i+2) { # n/2*1 = n/2 = O(n)
+#     statement;              # 1
+# }
 
 # Q4: What is the time complexity of
 # O(n^2), porque tiene for anidado
-for (i = 0; i < n; i++) {       # n*n = n^2 = O(n^2)
-    for (j = 0; j < n; j++) {   # n*1 = n
-        statement;              # 1
-    }
-}
+# for (i = 0; i < n; i++) {       # n*n = n^2 = O(n^2)
+#     for (j = 0; j < n; j++) {   # n*1 = n
+#         statement;              # 1
+#     }
+# }
 
 # Q5: What is the time complexity of
 # i   j                           iteraciones
@@ -131,11 +124,11 @@ for (i = 0; i < n; i++) {       # n*n = n^2 = O(n^2)
 #
 # 0 + 1 + 2 + 3 + ... + n = n*(n + 1) / 2
 # = O(n^2)
-for (i = 0; i < n; i++) {
-    for (j = 0; j < i; j++) {
-        statement;
-    }
-}
+# for (i = 0; i < n; i++) {
+#     for (j = 0; j < i; j++) {
+#         statement;
+#     }
+# }
 
 # Q6: What is the time complexity of
 # i   p
@@ -149,10 +142,10 @@ for (i = 0; i < n; i++) {
 # k^2 > n
 # k > n^(1/2)
 # O(n^(1/2))
-p = 0
-for (i = 1; p <= n; i++) {
-    p = p + i;
-}
+# p = 0
+# for (i = 1; p <= n; i++) {
+#     p = p + i;
+# }
 
 # Q7: What is the time complexity of
 # i = 1, 2, 4, 8, 16, ..., 2^k
@@ -160,9 +153,9 @@ for (i = 1; p <= n; i++) {
 # 2^k = n
 # k = log_2(n)
 # O(log_2(n))
-for (i = 1; i < n; i = i*2) {
-    statement;
-}
+# for (i = 1; i < n; i = i*2) {
+#     statement;
+# }
 
 # Q8: What is the time complexity of
 # i = n, n/(2^1), n/(2^2), n/(2^3), ..., n/(2^k)
@@ -171,9 +164,9 @@ for (i = 1; i < n; i = i*2) {
 # n = 2^k
 # log_2(n) = k
 # O(log_2(n))
-for (i = n; i >= 1; i = i/2) {
-    statement;
-}
+# for (i = n; i >= 1; i = i/2) {
+#     statement;
+# }
 
 # Q9: What is the time complexity of
 # i = 0^2, 1^2, 2^2, 3^3, ..., k^2
@@ -181,37 +174,37 @@ for (i = n; i >= 1; i = i/2) {
 # k^2 = n
 # k = n^(1/2)
 # O(n^(1/2))
-for (i = 0; i * i < n; i++) {
-    statement;
-}
+# for (i = 0; i * i < n; i++) {
+#     statement;
+# }
 
 # Q10: What is the time complexity of
 # n + n = 2n
 # O(2n) = O(n)
-for (i = 0; i < n; i++) { # n*1=n
-    statement;            # 1
-}
+# for (i = 0; i < n; i++) { # n*1=n
+#     statement;            # 1
+# }
 
-for (j = 0; j < n; j++) { # n*1=n
-    statement;            # 1
-}
-                          # n + n = 2n
+# for (j = 0; j < n; j++) { # n*1=n
+#     statement;            # 1
+# }
+                            # n + n = 2n
 
 # Q11: What is the time complexity of
 # O(log_2(log_2(n)))
-p = 0
-for (i = 1; i < n; i = i * 2) { # O(log_2(n))
-    p++;                        # p = log_2(n)
-}
+# p = 0
+# for (i = 1; i < n; i = i * 2) { # O(log_2(n))
+#     p++;                        # p = log_2(n)
+# }
 
-for (j = 1; j < p; j = j * 2) { # log_2(p) = log_2(log_2(n))
-    statement;
-}
+# for (j = 1; j < p; j = j * 2) { # log_2(p) = log_2(log_2(n))
+#     statement;
+# }
 
 # Q12: What is the time complexity of
 # O(n*log(n))
-for (i = 0; i < n; i++) {           # n*log_2(n) = O(n*log(n))
-    for (j = 1; j < n; j = j * 2) { # log_2(n)*1
-        statement;                  # 1
-    }
-}
+# for (i = 0; i < n; i++) {           # n*log_2(n) = O(n*log(n))
+#     for (j = 1; j < n; j = j * 2) { # log_2(n)*1
+#         statement;                  # 1
+#     }
+# }
