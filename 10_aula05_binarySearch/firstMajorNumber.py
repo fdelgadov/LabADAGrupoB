4707d8b6c00f8a7c48a5d52698a4254ebd18281a
fdelgadov <fdelgadov@unsa.edu.pe>
2021-11-22 19:47:23 -0500

    Aula05: Se mejora la correctitud de firstMajorNumber.py

--- 10_aula05_binarySearch/firstMajorNumber.py
+++ 10_aula05_binarySearch/firstMajorNumber.py
@@ -9,28 +9,29 @@ def firstMajorNumber(array, x):
   left = 0
   right = len(array) - 1
   while left <= right:
-    mid = left + (right - left) / 2
-    mid = int(mid)
+    mid = left + (right - left) // 2
     if array[mid] == x:
       return mid
     if array[mid] < x:
       left = mid + 1
     else:
       right = mid - 1
-  mid += 1
-  if len(array) <= mid:
+  if len(array) <= mid + 1:
     return -1
-  if mid == 1:
-    return 0
-  else:
+  if x < array[mid]:
     return mid
+  else:
+    return mid + 1
 
 # Test
 array = [2, 14, 23, 52, 65]
 print(array)
 print(f'firstMajorNumber(array, 100): {firstMajorNumber(array, 100)}')
+print(f'firstMajorNumber(array, 10): {firstMajorNumber(array, 10)}')
+
 array = [2, 14, 23, 52, 65, 100, 1233]
 print(array)
 print(f'firstMajorNumber(array, 70): {firstMajorNumber(array, 70)}')
 print(f'firstMajorNumber(array, 1): {firstMajorNumber(array, 1)}')
 print(f'firstMajorNumber(array, 23): {firstMajorNumber(array, 23)}')
+print(f'firstMajorNumber(array, 10): {firstMajorNumber(array, 10)}')
