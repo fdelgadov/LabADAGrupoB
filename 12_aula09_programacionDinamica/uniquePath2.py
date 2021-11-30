f5b24e0646458dd87092bfa5158bd1e848ccba76
fdelgadov <fdelgadov@unsa.edu.pe>
2021-11-30 09:38:45 -0500

    Aula09: Se completa el ejercicio uniquePath2
    - Se completa el algoritmo
    - Se evalúa el algoritmo
    - Se agrega la imagen

--- 12_aula09_programacionDinamica/uniquePath2.py
+++ 12_aula09_programacionDinamica/uniquePath2.py
@@ -3,53 +3,83 @@ def uniquePath2(obstacleGrid: list):
   Este algoritmo resuelve el siguiente ejercicio:
   Unique Path II
   https://leetcode.com/problems/unique-paths-ii/
+
+  Este algoritmo retorna el número de caminos únicos que puede seguir un robot
+  para llegar a la meta. El robot se ubica en la posición superior izquierda y
+  la meta en la posición inferior derecha de un arreglo. El arreglo
+  representa un mapa con caminos (0) y obstáculos (1).
   """
-  ROBOT = 2
-  GOAL = 3
+
   OBSTACLE = 1
-  ROAD = 1
-
   roads = [[0 for y in x] for x in obstacleGrid]
-  uniquePaths = 0
+  m = len(roads)
+  n = len(roads[0])
 
-  # Robot position
-  row = 0
-  col = 0
+  if obstacleGrid[0][0] == OBSTACLE:
+    return 0
+  if m == 1 and n == 1:
+    return 1
 
-  obstacleGrid[0][0] = ROBOT
-  obstacleGrid[-1][-1] = GOAL
+  roads[0][0] = 1
+  for col in range(1, n):
+    if obstacleGrid[0][col] != OBSTACLE:
+      left = roads[0][col - 1]
+      roads[0][col] = left
 
-  for col in range(len(roads[0])):
-    if row != 0 or roads[row - 1][col] == ROAD:
-      down = obstacleGrid[row][col]
+  for row in range(1, m):
+    for col in range(n):
+      if obstacleGrid[row][col] != OBSTACLE:
+        top = roads[row - 1][col]
+        left = roads[row][col - 1]
+        roads[row][col] = top + left
+  
+  return roads[-1][-1]
 
-      if down != OBSTACLE:
-        roads[row][col] = 1
+def display(a):
+  """
+  Este programa imprime de forma ordenada las filas y columnas de un arreglo
+  bidimensional.
+  """
+  for i in a:
+    print(i)
 
-      if obstacleGrid[row][col] == GOAL:
-        uniquePaths += 1
-
-  print(f'{row} {col}')
-
-"""
-  right = obstacleGrid[row][col + 1]
-  while right != OBSTACLE:
-    down = obstacleGrid[row + 1][col]
-
-    if down != OBSTACLE:
-      roads[row][col] = 1
-    else:
-      roads[row][col] = 0
-
-    col += 1
-    if col >= len(roads):
-      col = 0
-      row += 1
-    right = obstacleGrid[row][col]
-"""
 # Test
 obstacles = [
   [0, 0, 0],
   [0, 1, 0],
   [0, 0, 0]]
-uniquePath2(obstacles)
+display(obstacles)
+print(f'uniquePath2(obstacles): {uniquePath2(obstacles)}\n')
+
+obstacles = [
+  [0, 0, 0, 1, 1],
+  [0, 1, 0, 1, 0],
+  [0, 0, 0, 0, 0]]
+display(obstacles)
+print(f'uniquePath2(obstacles): {uniquePath2(obstacles)}\n')
+
+obstacles = [
+  [0, 0, 0, 0, 1],
+  [0, 1, 0, 0, 1],
+  [0, 1, 1, 0, 1],
+  [0, 0, 0, 0, 0]]
+display(obstacles)
+print(f'uniquePath2(obstacles): {uniquePath2(obstacles)}\n')
+
+obstacles = [
+  [0, 0, 0],
+  [0, 1, 0],
+  [0, 0, 0],
+  [1, 0, 1],
+  [0, 0, 0]]
+display(obstacles)
+print(f'uniquePath2(obstacles): {uniquePath2(obstacles)}\n')
+
+obstacles = [
+  [0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0],
+  [0, 0, 1, 1, 0],
+  [0, 0, 0, 1, 0],
+  [1, 1, 0, 0, 0]]
+display(obstacles)
+print(f'uniquePath2(obstacles): {uniquePath2(obstacles)}\n')
