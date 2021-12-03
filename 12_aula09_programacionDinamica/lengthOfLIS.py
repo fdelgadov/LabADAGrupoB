29a04db8b3177f156837d0b80ec21e02c52ced7f
fdelgadov <fdelgadov@unsa.edu.pe>
2021-12-03 14:36:22 -0500

    Aula09: Se reduce el uso de memoria de lengthOfLIS
    - Se reduce el uso de memoria usando un arreglo lineal y un contador

--- 12_aula09_programacionDinamica/lengthOfLIS.py
+++ 12_aula09_programacionDinamica/lengthOfLIS.py
@@ -1,29 +1,35 @@
 # Number of longest increasing subsequence
 
 def lengthOfLIS(nums):
-  LISArr = [[1, nums[-1]]]
+  """
+  Este programa resuelve el siguiente ejercicio: Longest Increasing Subsequence
+  Link: https://leetcode.com/problems/longest-increasing-subsequence/
+
+  Este programa retorna el tamaño de la subsecuencia ascendente de mayor tamaño
+  Una subsecuencia de un arreglo A es un arreglo formado por algunos elementos
+  de A sin alterar el orden.
+  """
+  LISArr = [nums[-1]]
   for num in nums[-2::-1]:
     update = False
 
+    i = len(LISArr)
     for LIS in LISArr[::-1]:
-      if num < LIS[1]:
-        length = LIS[0] + 1
+      if num < LIS:
+        length = i + 1
         if len(LISArr) < length:
-          LISArr.append([length, num])
-        elif num > LISArr[length - 1][1]:
-          LISArr[length - 1][0] = length
-          LISArr[length - 1][1] = num
+          LISArr.append(num)
+        elif num > LISArr[length - 1]:
+          LISArr[length - 1] = num
         update = True
         break
+      i -= 1
 
     if not update:
-      if LISArr[0][1] < num:
-        LISArr[0][1] = num
+      if LISArr[0] < num:
+        LISArr[0] = num
 
-  return LISArr[-1][0]
-      
-    
-
+  return len(LISArr)
 
 # Test
 nums = [10, 9, 2, 5, 3, 7, 101, 18]
