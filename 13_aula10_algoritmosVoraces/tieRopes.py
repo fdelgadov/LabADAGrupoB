144bc7c29dc2a0da54c33adcb89227d6f0e00b4a
fdelgadov <fdelgadov@unsa.edu.pe>
2021-12-11 15:56:37 -0500

    Aula10: Se agrega descripción a tieRopes

--- 13_aula10_algoritmosVoraces/tieRopes.py
+++ 13_aula10_algoritmosVoraces/tieRopes.py
@@ -1,18 +1,36 @@
 # Tie Ropes
 
 def tieRopes(K, A: list):
+  """
+  Este programa resuele el ejercicio Tie Ropes
+  Link: https://app.codility.com/programmers/lessons/16-greedy_algorithms/tie_ropes/
+
+  Dado una lista de cuerdas A y un entero K, el programa retonar el mayor 
+  número de cuerdas de tamaño >= K. Las cuerdas adyacientes pueden unirse 
+  en una nueva cuerda.
+  """
+
+  # Número de cuerdas >= K
   ropes = 0
 
+  # Se evalúan todas las cuerdas, menos la última
   while len(A) > 1:
+    # Se saca la primera cuerda
     actual = A.pop(0)
+
+    # Se compara la longitud de la cuerda con K
     if actual < K:
+      # Se une la cuerda sacada con la siguiente; forman una nueva cuerda
       A.insert(0, actual + A.pop(0))
     else:
+      # La cuerda es >= K, entonces aumenta el contador
       ropes += 1
   
+  # Se evalúa la última cuerda
   if A[0] >= K:
     ropes += 1
 
+  # Retorna la respuesta
   return ropes
 
 A = [1, 2, 3, 4, 1, 1, 3]
