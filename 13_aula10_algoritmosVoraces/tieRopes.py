cad293f62cf65e14185a53c8486806eb2efddb04
fdelgadov <fdelgadov@unsa.edu.pe>
2021-12-11 16:11:22 -0500

    Aula10: Se explica solución en tieRopes

--- 13_aula10_algoritmosVoraces/tieRopes.py
+++ 13_aula10_algoritmosVoraces/tieRopes.py
@@ -8,6 +8,13 @@ def tieRopes(K, A: list):
   Dado una lista de cuerdas A y un entero K, el programa retonar el mayor 
   número de cuerdas de tamaño >= K. Las cuerdas adyacientes pueden unirse 
   en una nueva cuerda.
+
+  Solucion:
+  El objetivo es hallar el mayor número de cuerdas >= K, teniendo en cuenta que
+  es posible unir cuerdas adyacentes. Para ello, es suficiente con sacar y unir
+  las cuerdas con tamaño < K según el orden en el que estan dispuestas en la
+  lista A, ya que solo pueden ser unidas si son adyacentes. Cada vez que es
+  sacada una cuerda >= K, es contada para el total de la respuesta final.
   """
 
   # Número de cuerdas >= K
